b971533e7df4f10bb9715065d465552e4257970f
icarus <gg.hilock@gmail.com>
2024-01-29 16:46:49 +0800

    y有人相爱，有人夜里看海，有人今天工资普涨

--- src/problems/T_2_AddTwoNumbers.java
+++ src/problems/T_2_AddTwoNumbers.java
@@ -160,5 +160,25 @@ public class T_2_AddTwoNumbers {
         return  pre.next;
     }
 
+    public static ListNode solution5 (ListNode l1, ListNode l2) {
+        ListNode pre = new ListNode(0);
+        ListNode cur = pre;
+        int addN = 0;
+        while (null != l1 || null != l2 || addN != 0) {
+            int x = l1 == null ? 0 : l1.val;
+            int y = l2 == null ? 0 : l2.val;
+            int sum = x + y + addN;
+            addN = sum / 10;
+            cur.next = new ListNode(sum % 10);
+            cur = cur.next;
+            if (null != l1) {
+                l1 = l1.next;
+            }
+            if (null != l2) {
+                l2 = l2.next;
+            }
+        }
+        return pre.next;
+    }
 
 }
